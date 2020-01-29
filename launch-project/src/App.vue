0b66dcb22ec239f5bc58d985e5268e6fd0f4a475
Hélène Meneuvrier <helene@wedogood.co>
2020-01-29 14:26:35 +0100

    Asana 1154426352284186 : affichage des valeurs par défaut, intégration du composant WDGForm, intégration de la structure vue create pour être cohérent

--- launch-project/src/App.vue
+++ launch-project/src/App.vue
@@ -1,12 +1,56 @@
 <template>
   <div id="app">
-    <img src="https://www.wedogood.co/wp-content/themes/yproject/images/navbar/logo-wdg.png"><br>
-    <router-view/>
+    <!-- <router-view/> -->
+    <LaunchProject :firstname="firstname"  :lastname="lastname"  :email="email"  :phonenumber="phonenumber"  :organame="organame"  :projectname="projectname"  :projectdescription="projectdescription" />
   </div>
 </template>
 
 <script>
+
+import LaunchProject from './components/LaunchProject.vue'
+const initElements = document.querySelector('#app')
+
 export default {
-  name: 'App'
+  name: 'app',
+	data () {
+		return {
+			firstname: '',
+			lastname: '',
+			phonenumber: '',
+			organame: '',
+			email: '',
+			projectname: '',
+			projectdescription: ''
+		}
+	},
+	components: {
+		LaunchProject
+	},
+	created () {
+		this.firstname = initElements.dataset.firstname
+		this.lastname = initElements.dataset.lastname
+		this.phonenumber = initElements.dataset.phonenumber
+		this.email = initElements.dataset.email
+		this.organame = initElements.dataset.organame
+		this.projectname = initElements.dataset.projectname
+		this.projectdescription = initElements.dataset.projectdescription
+	}
 }
 </script>
+
+<style>
+#app {
+  font-family: Trebuchet MS, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+}
+h1 {
+	text-align: center;
+	margin: 0;
+	font-family: Trebuchet MS,sans-serif;
+	font-size: 28px;
+	font-weight: 700;
+	letter-spacing: .125em;
+}
+</style>
--- launch-project/src/components/LaunchProject.vue
+++ launch-project/src/components/LaunchProject.vue
@@ -3,7 +3,7 @@
 	<h1>Lancement de levée de fonds</h1>
 	<p>
 		Vous avez déjà créé un (ou des) projet(s) sur la plateforme précédemment :<br>
-		  <!-- Utiliser un composant pour générer la liste ? -->
+		<!-- Utiliser un composant pour générer la liste ? -->
         - Projet de création Hélène
 	</p>
 
@@ -11,32 +11,38 @@
         Si vous souhaitez tout de même créer un nouveau projet, veuillez remplir le formulaire ci-dessous.
     </p>
 
-    <!-- Créer un composant WDGInput, avec label, obligatoire ou pas, foction de validation des données, champ multiligne ou pas, commentaire -->
-    <WDGInput labelInput="Mon prénom" idInput="firstname" nameInput="firstname" v-bind:multiline="false" v-bind:optional="false" validation="is_name" valueInput="Hélène"></WDGInput>
-	<br>
-    <WDGInput labelInput="Mon nom" idInput="lastname" nameInput="lastname" v-bind:multiline="false" v-bind:optional="false" validation="is_name"></WDGInput>
-	<br>
-    <WDGInput labelInput="Mon téléphone mobile" idInput="phone_number" nameInput="phone_number" v-bind:multiline="false" v-bind:optional="false" validation="is_phone_number"></WDGInput>
-	<br>
-    <WDGInput labelInput="Nom de mon entreprise" idInput="company_name" nameInput="company_name" v-bind:multiline="false" v-bind:optional="false" validation="is_name"></WDGInput>
-	<br>
-    <WDGInput labelInput="E-mail de contact" idInput="email" nameInput="email" v-bind:multiline="false" v-bind:optional="false" validation="is_email" comment="Cette adresse doit être différente de celle de votre compte personnel, utilisez une adresse telle que contact@votre-entreprise.fr"></WDGInput>
-	<br>
-    <WDGInput labelInput="Nom du projet" idInput="project_name" nameInput="project_name" v-bind:multiline="false" v-bind:optional="false" validation="is_name"></WDGInput>
-	<br>
-    <WDGInput labelInput="Description du projet" idInput="project_description" nameInput="project_description" v-bind:multiline="true" v-bind:optional="false"></WDGInput>
-	<br>
+	<WDGForm formName="form-launch-project" formAction="/launch-project/" v-bind:hasFiles="false">
+		<!-- Créer un composant WDGInput, avec label, obligatoire ou pas, foction de validation des données, champ multiligne ou pas, commentaire -->
+		<WDGInput labelInput="Mon prénom" idInput="firstname" nameInput="firstname" v-bind:multiline="false" v-bind:optional="false" validation="is_name" :valueInput=firstname></WDGInput>
+		<br>
+		<WDGInput labelInput="Mon nom" idInput="lastname" nameInput="lastname" v-bind:multiline="false" v-bind:optional="false" validation="is_name" :valueInput=lastname></WDGInput>
+		<br>
+		<WDGInput labelInput="Mon téléphone mobile" idInput="phone_number" nameInput="phone_number" v-bind:multiline="false" v-bind:optional="false" validation="is_phone_number"  :valueInput=phonenumber></WDGInput>
+		<br>
+		<WDGInput labelInput="Nom de mon entreprise" idInput="company_name" nameInput="company_name" v-bind:multiline="false" v-bind:optional="false" validation="is_name" :valueInput=organame></WDGInput>
+		<br>
+		<WDGInput labelInput="E-mail de contact" idInput="email" nameInput="email" v-bind:multiline="false" v-bind:optional="false" validation="is_email"  :valueInput=email comment="Cette adresse doit être différente de celle de votre compte personnel, utilisez une adresse telle que contact@votre-entreprise.fr"></WDGInput>
+		<br>
+		<WDGInput labelInput="Nom du projet" idInput="project_name" nameInput="project_name" v-bind:multiline="false" v-bind:optional="false" validation="is_name"  :valueInput=projectname></WDGInput>
+		<br>
+		<WDGInput labelInput="Description du projet" idInput="project_description" nameInput="project_description" v-bind:multiline="true" v-bind:optional="false"  :valueInput=projectdescription></WDGInput>
+		<br>
 
-    <!-- Créer un composant WDGCheckbox -->
-    <WDGCheckbox labelCheckbox="Je valide les conditions particulières" v-bind:optional="false"></WDGCheckbox>
-   
-    <!-- Créer un composant bouton -->
-    <WDGButton labelButton="Valider"></WDGButton>
+		<!-- Créer un composant WDGCheckbox -->
+		<WDGCheckbox labelCheckbox="Je valide les conditions particulières" v-bind:optional="false"></WDGCheckbox>
+
+		<div>
+			* Champs obligatoires
+		</div>
+		<!-- Créer un composant bouton -->
+		<WDGButton labelButton="Valider"></WDGButton>
+	</WDGForm>
 
   </div>
 </template>
 
 <script>
+import WDGForm from '@/../../common/src/components/WDGForm'
 import WDGInput from '@/../../common/src/components/WDGInput'
 import WDGCheckbox from '@/../../common/src/components/WDGCheckbox'
 import WDGButton from '@/../../common/src/components/WDGButton'
@@ -44,22 +50,27 @@ import WDGButton from '@/../../common/src/components/WDGButton'
 export default {
 	name: 'LaunchProject',
 	components: {
-        WDGInput,
-        WDGCheckbox,
+		WDGForm,
+		WDGInput,
+		WDGCheckbox,
 		WDGButton
 	},
-	data () {
-	},
-	methods: {
-	},
-	mounted(){
-		//this.initListYear();
-	}
+	props: ['firstname', 'lastname', 'phonenumber', 'organame', 'email', 'projectname', 'projectdescription']
 }
 </script>
 
 <style>
 	.clear {
 		clear: both;
+	}
+	.launch-project {
+		width: 480px;
+		margin: auto;
+	}
+	@media screen and (max-width:767px) {
+		.launch-project {
+			width: calc( 100% - 4px );
+			margin: 0px 2px;
+		}
 	}
 </style>
