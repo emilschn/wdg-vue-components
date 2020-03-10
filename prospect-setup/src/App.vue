07ce05690fca6e992a664c1b2cfbb6d668dc5e3e
emilschn <emilien.schneider@poucr.net>
2020-03-10 18:01:16 +0100

    WDGTabs : création et début mise en page

--- prospect-setup/src/App.vue
+++ prospect-setup/src/App.vue
@@ -3,25 +3,48 @@
 		<WDGHeader>
 			<slot slot="title">{{ $t('project-setup.TITLE') }}</slot>
 		</WDGHeader>
-			<router-view></router-view>
+
+		<WDGTabs :tabItems="tabItems" :currentTab="currentRoute" v-if="canShowTabs" />
+
+		<router-view></router-view>
+
 		<WDGFooter />
 	</div>
 </template>
 
 <script>
+import i18n from '@/i18n'
+import WDGTabs from '@/../../common/src/components/WDGTabs'
 import WDGHeader from '@/../../common/src/components/WDGHeader'
 // import TheScreenIntro from './components/TheScreenIntro.vue'
 // import TheScreenProjectInfos from './components/TheScreenProjectInfos.vue'
 import WDGFooter from '@/../../common/src/components/WDGFooter'
 
 export default {
-  name: 'App',
-  components: {
-	WDGHeader,
-    // TheScreenIntro,
-	// TheScreenProjectInfos,
-	WDGFooter
-  }
+	name: 'App',
+	components: {
+		WDGTabs,
+		WDGHeader,
+		WDGFooter
+	},
+	data () {
+		return {
+			tabItems: [
+				{ Id: 'project-infos', Label: i18n.t('project-setup.tabs.MY_PROJECT'), Index: '1', Subtitle: 'Compléter', Status: 'incomplete' },
+				{ Id: 'my-funding', Label: i18n.t('project-setup.tabs.MY_FUNDING'), Index: '2', Subtitle: 'Compléter' },
+				{ Id: 'my-investors', Label: i18n.t('project-setup.tabs.MY_INVESTORS'), Index: '3', Subtitle: 'Compléter' },
+				{ Id: 'my-result', Label: i18n.t('project-setup.tabs.MY_RESULT'), Subtitle: 'En cours...' }
+			]
+		}
+	},
+	computed: {
+		canShowTabs () {
+			return (this.$route.name !== 'intro')
+		},
+		currentRoute () {
+			return this.$route.name
+		}
+	}
 }
 </script>
 
