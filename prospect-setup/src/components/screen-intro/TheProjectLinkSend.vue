67a76cc33cd23f18084459a65c4fa0598710539d
emilschn <emilien.schneider@poucr.net>
2020-04-03 14:41:15 +0200

    Prospect setup : envoi de mail standard pour liste des tests démarrés

--- prospect-setup/src/components/screen-intro/TheProjectLinkSend.vue
+++ prospect-setup/src/components/screen-intro/TheProjectLinkSend.vue
@@ -1,32 +1,33 @@
 <template>
 	<div class="the-project-link-send">
 		<div class="the-project-link-send-mail" v-if="canShowSendMail == true">
-            <br>
-            <div class="text">{{ sendText }}</div>
-            <WDGForm
-                name="form-signin"
-                :action="ajaxUrl"
-                :onSubmitEvent="formSubmit"
-                v-bind:hasFiles="false"
-                :errorFeedback="errorFeedback"
-                :successFeedback="successFeedback"
-            >
-                <WDGInput
-                    :placeholder="$t('project-setup.project-infos.PLACEHOLDER_USER_EMAIL')"
-                    customStyle="natural-language"
-			        validationRule="email"
-				/>
+			<div class="text">{{ sendText }}</div>
+			<WDGForm
+			  name="form-signin"
+			  :action="ajaxUrl"
+			  :onSubmitEvent="formSubmit"
+			  v-bind:hasFiles="false"
+			  :errorFeedback="errorFeedback"
+			  :successFeedback="successFeedback"
+			  >
+				<WDGInput
+				  :value="sharedState.user.email"
+				  v-bind:valueReturn.sync="sharedState.user.email"
+				  :placeholder="$t('project-setup.project-infos.PLACEHOLDER_USER_EMAIL')"
+				  customStyle="natural-language"
+				  validationRule="email"
+				  />
 				<WDGButton
-				    color="blue"
-				    type="submit"
-			        v-bind:disabled="loading"
-				>
+				  color="blue"
+				  type="submit"
+				  v-bind:disabled="loading"
+				  >
 					<slot slot="label">{{ $t('common.SEND') }}</slot>
 				</WDGButton>
-            </WDGForm>
+			</WDGForm>
 		</div>
 		<div class="the-project-link-send-title" v-on:click="showMail" v-else>
-            <u>{{ $t('project-setup.intro.the-project-link-send.TITLE') }}</u>
+			<u>{{ $t('project-setup.intro.the-project-link-send.TITLE') }}</u>
 		</div>
 	</div>
 </template>
@@ -34,6 +35,7 @@
 <script>
 import axios from 'axios'
 import i18n from '@/i18n'
+import { store } from '../../store.js'
 import WDGForm from '@/../../common/src/components/WDGForm'
 import WDGInput from '@/../../common/src/components/WDGInput'
 import WDGButton from '@/../../common/src/components/WDGButton'
@@ -50,45 +52,53 @@ export default {
     },
 	data () {
 		return {
-			email: '',
+			sharedState: store.state,
+			sharedProps: store.props,
+			errorFeedback: '',
+			successFeedback: '',
 			canShowSendMail: false,
 			loading: false,
 			sendText: i18n.t('project-setup.intro.the-project-link-send.TEXT')
         }
     },
-    methods: {
-        showMail: function (event) {
-            this.canShowSendMail = true
-        },
-        formSubmit () {
-            this.loading = true
-            if (this.email === '') {
-                this.sendText = i18n.t('project-setup.intro.the-project-link-send.ERROR_EMAIL_EMPTY')
-            } else {
-                let data = new FormData()
-                data.append('action', 'send_user_draft_projects')
-                data.append('email', this.email)
+	methods: {
+		showMail: function (event) {
+			this.canShowSendMail = true
+		},
+		formSubmit () {
+			this.errorFeedback = ''
+			this.successFeedback = ''
+			if (this.sharedState.user.email === '') {
+				this.errorFeedback = i18n.t('project-setup.intro.the-project-link-send.ERROR_EMAIL_EMPTY')
+			} else {
+				this.loading = true
+				let data = new FormData()
+				data.append('action', 'prospect_setup_send_mail_user_project_drafts')
+				data.append('email', this.sharedState.user.email)
 
-                axios
-                    .post (this.ajaxUrl, data)
-                    .then (response => {
-                        let responseData = response.data
-                        if (responseData.has_error === '1') {
-                            this.sendText = i18n.t(getErrorMessage(responseData.error_str))
-                            this.loading = false
-                        } else {
-                            this.sendText = i18n.t('project-setup.intro.the-project-link-send.EMAIL_SENT')
-                        }
-                    })
-                    .catch (error => {
-                        console.log(error)
-                        this.loading = false
-                    })
-                    .finally (() => {
-                    })
-            }
-        }
-    }
+				axios
+					.post (this.sharedProps.ajaxurl, data)
+					.then (response => {
+						let responseData = response.data
+						if (responseData.has_error === '1') {
+							this.errorFeedback = i18n.t(getErrorMessage(responseData.error_str))
+						} else {
+							if (responseData.email_sent === '1') {
+								this.successFeedback = i18n.t('project-setup.intro.the-project-link-send.EMAIL_SENT')
+							} else {
+								this.errorFeedback = i18n.t('project-setup.intro.the-project-link-send.EMAIL_SEND_ERROR')
+							}
+						}
+					})
+					.catch (error => {
+						console.log(error)
+					})
+					.finally (() => {
+						this.loading = false
+					})
+			}
+		}
+	}
 }
 function getErrorMessage (errorCode) {
 	switch (errorCode) {
@@ -106,36 +116,40 @@ function getErrorMessage (errorCode) {
 
 <style>
 .the-project-link-send-title {
+	font-size: 12px;
 	text-align: center;
 	color: #B4B4B4;
-	font-size: 12px;
-    cursor: pointer;
+	cursor: pointer;
 }
+
 .the-project-link-send-mail {
 	background: #f6f6f6;
-    margin: 0px auto;
-    width: 715px;
-    height: 180px;
+	width: 715px;
+	margin: 0px auto;
+	padding: 16px;
 }
 .the-project-link-send-mail .text {
 	text-align: center;
 }
 .the-project-link-send-mail .wdg-form {
-    margin-top: 20px;
+	margin: 16px 0px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-wrap: wrap;
 	text-align: center;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
 }
-.the-project-link-send-mail .wdg-button {
-    width: 140px;
-    margin-top: 0px;
-    margin-bottom: 0px;
+.the-project-link-send-mail .wdg-form > span {
+	flex: 0 0 100%;
 }
 .the-project-link-send-mail .wdg-input {
-    width: 246px;
-    margin-top: 0px;
-    margin-bottom: 0px;
+	width: 246px;
+	margin-top: 0px;
+	margin-bottom: 0px;
 }
-
+.the-project-link-send-mail .wdg-button {
+	width: 140px;
+	margin-top: 0px;
+	margin-bottom: 0px;
+}
 </style>
