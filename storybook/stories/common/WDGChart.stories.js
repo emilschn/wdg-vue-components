391639d5ecd0e3639bccf2e06c8f0751154207f3
emilschn <emilien.schneider@poucr.net>
2020-06-29 17:32:29 +0200

    Storybook : story pour Chartjs en disant qu'on ne teste pas en interne

---
+++ storybook/stories/common/WDGChart.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'WDGChart'
+};
+
+export const Empty = () => ({
+	template: '<div style="text-align: center;">Tester revient à tester chart-js. On ne le fait pas en interne</div>'
+});
