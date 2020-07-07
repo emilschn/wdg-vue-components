2fb6d288a4a61ea1d553b6305a624836009ebc89
emilschn <emilien.schneider@poucr.net>
2020-07-07 14:26:50 +0200

    Storybook : WDGTabs : mise en place exemple par défaut

--- storybook/stories/common/WDGTabs.stories.js
+++ storybook/stories/common/WDGTabs.stories.js
@@ -5,7 +5,18 @@ export default {
   component: WDGTabs,
 };
 
+const tabItems = [
+	{ Id: '1', Index: '1', Status: 'incomplete', Label: 'First label' },
+	{ Id: '2', Index: '2', Status: 'incomplete', Label: 'Second label', LinkLabel: 'My link' },
+	{ Id: '3', Index: '3', Status: 'incomplete', Label: 'Third label', Subtitle: 'Third subtitle' },
+];
+
 export const Default = () => ({
   components: { WDGTabs },
-  template: '<WDGTabs class="wdg-tabs"><ul><li><div><svg width="48" height="48"><image xlink:href="@/../../common/src/assets/icons/incomplete.svg" src="@/../../common/src/assets/icons/incomplete.png" width="48" height="48" /></svg><svg width="48" height="48"><image xlink:href="@/../../common/src/assets/icons/complete.svg" src="@/../../common/src/assets/icons/complete.png" width="48" height="48" /></svg><span class="tab-label"><span v-if="tabItem.Index"></span><span class="full-size"></span><span class="responsive"><span><a></a></span><span></span></span><span class="subtitle"></span><a></a></span><svg width="25" height="25" class="points-menu"><image xlink:href="@/../../common/src/assets/icons/points-menu.svg" src="@/../../common/src/assets/icons/points-menu.png" width="25" height="25" /></svg></div></li></ul></WDGTabs>',
+  template: '<WDGTabs :tabItems="tabItems"></WDGTabs>',
+  props: {
+	tabItems: {
+		default: () => tabItems
+	}
+  },
 });
