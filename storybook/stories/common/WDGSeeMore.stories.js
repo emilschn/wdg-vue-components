88174b3540965109ea51955dd06493095fc2d37e
emilschn <emilien.schneider@poucr.net>
2020-07-07 14:22:12 +0200

    Storybook : WDGSeeMore : mise en place deux premiers cas

--- storybook/stories/common/WDGSeeMore.stories.js
+++ storybook/stories/common/WDGSeeMore.stories.js
@@ -6,8 +6,34 @@ export default {
   component: WDGSeeMore,
 };
 
+const checkedItems = [
+	'checked item 1',
+	'checked item 2',
+	'checked item 3'
+];
+
+const uncheckedItems = [
+	'unchecked item 1',
+	'unchecked item 2',
+	'unchecked item 3'
+];
+
 export const Default = () => ({ // comment faire pour faire dérouler un texte comme sur l'IP?
   components: { WDGSeeMore },
-  template: '<WDGSeeMore><div class="wdg-see-more"><div class="see-more-title"><slot name="title">Voir plus</slot></div></div></WDGSeeMore>',
+  template: '<WDGSeeMore><slot slot="title">My Title</slot><slot slot="text">My text</slot></WDGSeeMore>',
   i18n,
+});
+
+export const Items = () => ({ // comment faire pour faire dérouler un texte comme sur l'IP?
+  components: { WDGSeeMore },
+  template: '<WDGSeeMore :checkedItems="checkedItems" :uncheckedItems="uncheckedItems"><slot slot="title">My Title</slot><slot slot="text">My text</slot></WDGSeeMore>',
+  i18n,
+  props: {
+	checkedItems: {
+		default: () => checkedItems
+	},
+	uncheckedItems: {
+		default: () => uncheckedItems
+	}
+  },
 });
