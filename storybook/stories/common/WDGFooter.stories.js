3b365837e706b7fb213f89499b68cc1e28f01bb3
emilschn <emilien.schneider@poucr.net>
2020-07-07 12:00:02 +0200

    Storybook : ajout WDGForm commenté (non fonctionnel) et correction WDGFooter pour fonctionnement

--- storybook/stories/common/WDGFooter.stories.js
+++ storybook/stories/common/WDGFooter.stories.js
@@ -1,4 +1,5 @@
 import WDGFooter from './../../../common/src/components/WDGFooter';
+import { i18n } from '../../src/plugins/i18n';
 
 export default {
   title: 'WDGFooter',
@@ -6,7 +7,7 @@ export default {
 };
 
 export const Default = () => ({
-    components: { WDGFooter },
-    template: '<WDGFooter><div class="footer"></div></WDGFooter>',
-  });
-    
+	components: { WDGFooter },
+	template: '<WDGFooter><div class="footer"></div></WDGFooter>',
+	i18n,
+});
---
+++ storybook/stories/common/WDGForm.stories.js
@@ -0,0 +1,14 @@
+/*import WDGForm from './../../../common/src/components/WDGForm';
+import { i18n } from '../../src/plugins/i18n';
+
+
+export default {
+  title: 'WDGForm',
+  component: WDGForm,
+};
+
+export const Default = () => ({
+	components: { WDGForm },
+	template: '<WDGForm></WDGForm>',
+	i18n,
+});*/
