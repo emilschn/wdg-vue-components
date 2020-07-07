03a83d21bafe48371523f65f5fc8ddf8d0323b2f
emilschn <emilien.schneider@poucr.net>
2020-07-07 11:37:57 +0200

    Storybook : Mascotte : intégration des 5 de bases + 2 avec texte

--- storybook/stories/common/WDGMascot.stories.js
+++ storybook/stories/common/WDGMascot.stories.js
@@ -5,7 +5,37 @@ export default {
   component: WDGMascot,
 };
 
-export const Default = () => ({
+export const Face1 = () => ({
   components: { WDGMascot },
-  template: '<WDGMascot><div class="wdg-mascot"><div><slot name="text">Coucou</slot></div><svg width="281" height="256" v-if="type == \'face-1\'"><image xlink:href="@./../../../common/src/assets/mascot/front/1.svg" src="@./../../../common/src/assets/mascot/front/1.png" width="281" height="256" /></svg></div></WDGMascot>',
+  template: '<WDGMascot type="face-1"></WDGMascot>',
+});
+
+export const Face1ShortText = () => ({
+  components: { WDGMascot },
+  template: '<WDGMascot type="face-1"><slot slot="text">Hello !</slot></WDGMascot>',
+});
+
+export const Face1LongText = () => ({
+  components: { WDGMascot },
+  template: '<WDGMascot type="face-1"><slot slot="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at sagittis est, ut pharetra lectus. Aliquam accumsan quam ut auctor aliquet. Fusce dolor mi, mattis vel scelerisque non, vulputate et velit. In varius, diam ac sagittis tincidunt, ex neque aliquet orci, eget blandit metus augue sed tellus. Vestibulum tincidunt arcu. </slot></WDGMascot>',
+});
+
+export const Face2 = () => ({
+  components: { WDGMascot },
+  template: '<WDGMascot type="face-2"></WDGMascot>',
+});
+
+export const Face3 = () => ({
+  components: { WDGMascot },
+  template: '<WDGMascot type="face-3"></WDGMascot>',
+});
+
+export const Side1 = () => ({
+  components: { WDGMascot },
+  template: '<WDGMascot type="side-1"></WDGMascot>',
+});
+
+export const Side2 = () => ({
+  components: { WDGMascot },
+  template: '<WDGMascot type="side-2"></WDGMascot>',
 });
