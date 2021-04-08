4c905f78cdaa76c9a1c47f148c2f7557c7294478
Heloise <heloise.perat@etudiant.univ-lr.fr>
2021-04-08 16:47:49 +0200

    WDGTabs : refonte totale

--- common/src/components/WDGTabs.vue
+++ common/src/components/WDGTabs.vue
@@ -7,13 +7,9 @@
 			  :class="{ selected: (tabItem.Id == currentTab), expanded: isExpanded }"
 			  >
 				<div @click="onClickMenu(tabItem.Id)">
-					<svg width="48" height="48" v-if="tabItem.Status == 'incomplete'">
+					<!-- <svg width="48" height="48" v-if="tabItem.Status == 'incomplete'">
 						<image xlink:href="@/../../common/src/assets/icons/incomplete.svg" src="@/../../common/src/assets/icons/incomplete.png" width="48" height="48" />
-					</svg>
-
-					<svg width="48" height="48" v-if="tabItem.Status == 'complete'">
-						<image xlink:href="@/../../common/src/assets/icons/complete.svg" src="@/../../common/src/assets/icons/complete.png" width="48" height="48" />
-					</svg>
+					</svg> -->
 
 					<span class="tab-label">
 						<span v-if="tabItem.Index">{{ tabItem.Index }}.&nbsp;</span>
@@ -25,6 +21,11 @@
 						<span class="subtitle" v-if="tabItem.Subtitle">{{ tabItem.Subtitle }}</span>
 						<a @click="onClickLocalEvent(tabItem.Id)" v-if="tabItem.LinkLabel">{{ tabItem.LinkLabel }}</a>
 					</span>
+
+					<svg width="20" height="20" v-if="tabItem.Status == 'complete'">
+						<image xlink:href="@/../../common/src/assets/icons/check-grey.svg" src="@/../../common/src/assets/icons/check-grey.png" width="20" height="20"/>
+					</svg>
+
 					<svg width="25" height="25" class="points-menu"><image xlink:href="@/../../common/src/assets/icons/points-menu.svg" src="@/../../common/src/assets/icons/points-menu.png" width="25" height="25" /></svg>
 				</div>
 			</li>
@@ -68,58 +69,55 @@ export default {
 	margin-top: -1px;
 }
 .wdg-tabs ul {
-	display: table;
+	display: flex;
+	justify-content: space-between;
 	width: 100%;
-	height: 48px;
 	padding: 0px;
 	margin: 0px;
 }
 .wdg-tabs ul li {
 	display: table-cell;
 	width: 25%;
-	padding: 16px 0px;
+	padding: 20px 16px;
 	margin: 0px;
 	list-style: none;
-	border-bottom: 4px solid #B4B4B4;
-	background: #f4f2f2;
-	color: #B4B4B4;
+	border-bottom: 1px solid #C2C2C2;
+	color: #C2C2C2;
 }
 .wdg-tabs ul li.selected {
 	background: #fff;
-	border-bottom: 4px solid #00879B;
+	border-bottom: 4px solid #B3DAE1;
 	border-right: 0px;
 	color: #333;
 }
 .wdg-tabs ul li div {
-	height: 52px;
-	border-right: 1px solid #B4B4B4;
-}
-.wdg-tabs ul li.selected div, .wdg-tabs ul li:last-child div {
-	border-right: 0px;
+	display: flex;
 }
 .wdg-tabs ul li div svg {
-	float: left;
-	margin-left: 8px;
+	margin: 0px 10px;
 }
 .wdg-tabs ul li div span.tab-label {
-	margin-left: 10px;
-	font-weight: 600;
+	font-weight: 300;
 	font-size: 17px;
-	font-family: 'Trebuchet MS', sans-serif;
+}
+.wdg-tabs ul li.selected div span.tab-label {
+	font-weight: 600;
 }
 .wdg-tabs ul li div span.tab-label span.subtitle, .wdg-tabs ul li div span.tab-label a {
 	display: block;
 	margin-top: 8px;
-	margin-left: 65px;
-	font-size: 13px;
-	font-weight: 400;
-	color: #B4B4B4;
+	font-size: 15px;
+	font-weight: 600;
+	color: #333;
+	text-transform: none;
+	font-family: 'Arial', sans-serif;
+
 }
 .wdg-tabs ul li div span.tab-label span.subtitle {
 	text-decoration: none;
 }
 .wdg-tabs ul li div span.tab-label:last-of-type .subtitle {
-	margin-left: 10px;
+	color: #C2C2C2;
 }
 .wdg-tabs ul li div span.tab-label a {
 	text-decoration: underline;
@@ -149,12 +147,12 @@ div.wdg-tabs li.selected div span.tab-label a {
 	.wdg-tabs ul li {
 		display: none;
 		width: 100%;
-		border-bottom: 1px solid #B4B4B4;
-		padding: 10px 5px;
+		border-bottom: 1px solid #C2C2C2;
+		padding: 16px 5px;
 	}
 	.wdg-tabs ul li.selected {
 		display: block;
-		border-bottom: 4px solid #00879B;
+		border-bottom: 4px solid #B3DAE1;
 	}
 	.wdg-tabs ul li.selected div {
 		border-right: none;
@@ -173,9 +171,6 @@ div.wdg-tabs li.selected div span.tab-label a {
 	.wdg-tabs ul li div {
 		border-right: none;
 	}
-	.wdg-tabs ul li div span.tab-label {
-		font-size: 18px;
-	}
 	.wdg-tabs ul li div span.tab-label a {
 		display: none;
 	}
@@ -184,13 +179,13 @@ div.wdg-tabs li.selected div span.tab-label a {
 		margin: 0px;
 		font-weight: 600;
 		font-size: 17px;
-	}
-	.wdg-tabs ul li div span.tab-label span.subtitle {
-		margin-left: 20px;
+		font-family: 'Trebuchet MS', sans-serif;
+		text-decoration: none;
+		text-transform: uppercase;
 	}
 	.wdg-tabs svg, .wdg-tabs image {
-		width: 35px;
-		height: 35px;
+		width: 20px;
+		height: 20px;
 	}
 	.wdg-tabs .points-menu, .wdg-tabs .points-menu image {
 		display: block;
