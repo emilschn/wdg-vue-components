81f23e97fc794be4edec17cbe750e4d5c735f880
emilschn <emilien.schneider@poucr.net>
2021-06-15 11:51:57 +0200

    Changement des couleurs des bordures

---
+++ account-authentication/src/components/TheScreenInvestorType.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="the-screen-investor-type">
+		<WDGMascot type="face-1" alignMascot="left" :displayText="false" />
+		<div class="question">
+			<div class="question-bg">
+				{{ $t('account-authentication.INVEST_FOR_ORGANIZATION') }}
+			</div>
+			<WDGRadioButton
+			  name="need-orga"
+			  value="1"
+			  :groupValue="userNeedOrga"
+			  :onChange="onNeedOrgaChangeLocalEvent"
+			  >
+				<slot slot="label-after">{{ $t('common.YES') }}</slot>
+			</WDGRadioButton>
+			<WDGRadioButton
+			  name="need-orga"
+			  value="0"
+			  :groupValue="userNeedOrga"
+			  :onChange="onNeedOrgaChangeLocalEvent"
+			  >
+				<slot slot="label-after">{{ $t('common.NO') }}</slot>
+			</WDGRadioButton>
+
+			<WDGMessage
+			  v-if="userNeedOrga === '1'"
+			  iconSVG="warning.svg"
+			  iconColor="pink"
+			  >
+				<slot slot="label">{{ $t('account-authentication.YOU_MUST_REPRESENT_ORGANIZATION') }}</slot>
+			</WDGMessage>
+
+			<WDGButton
+			  v-if="userNeedOrga !== ''"
+			  color="red"
+			  type="button"
+			  :clickEvent="onButtonConfirmInvestorTypeEvent"
+			  >
+				<slot slot="label">{{ $t('common.CONTINUE') }}</slot>
+			</WDGButton>
+		</div>
+	</div>
+</template>
+
+<script>
+import WDGMascot from '@/../../common/src/components/WDGMascot'
+import WDGRadioButton from '@/../../common/src/components/WDGRadioButton'
+import WDGMessage from '@/../../common/src/components/WDGMessage'
+import WDGButton from '@/../../common/src/components/WDGButton'
+export default {
+	name: 'TheScreenInvestorType',
+	components: {
+		WDGMascot,
+		WDGRadioButton,
+		WDGMessage,
+		WDGButton
+	},
+	props: {
+		onConfirmUserType: Function
+	},
+	data () {
+		return {
+			userNeedOrga: ""
+		}
+	},
+	methods: {
+		onNeedOrgaChangeLocalEvent (newValue) {
+			this.userNeedOrga = newValue
+		},
+		onButtonConfirmInvestorTypeEvent () {
+			this.onConfirmUserType(this.userNeedOrga)
+		}
+	}
+}
+</script>
+
+<style>
+	div.the-screen-investor-type {
+		margin: 50px 0px;
+	}
+	div.the-screen-investor-type div.question {
+		float: left;
+		width: 500px;
+	}
+	div.the-screen-investor-type div.question-bg {
+		padding: 50px;
+		background: #ebebeb;
+	}
+</style>
