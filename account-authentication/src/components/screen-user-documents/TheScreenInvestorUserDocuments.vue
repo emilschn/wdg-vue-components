ee4ccc4b4206b987b8304b9e572f397a0bf5eaab
emilschn <emilien.schneider@poucr.net>
2021-08-06 09:48:18 +0200

    Account authentification : front de la partie sélection de document utilisateur

--- account-authentication/src/components/screen-user-documents/TheScreenInvestorUserDocuments.vue
+++ account-authentication/src/components/screen-user-documents/TheScreenInvestorUserDocuments.vue
@@ -1,65 +1,143 @@
 <template>
 	<div class="the-screen-investor-user-documents">
 
-		<div class="button-container" 
-			v-if="!isDocumentChosen"
-			>
+		<div
+		  v-if="isFileTypeDisplayed"
+		  class="button-container"
+		  >
 			<div class="title-container">
 				{{ $t('account-authentication.user-documents.CHOOSE_A_DOCUMENT_TO_SEND') }}
 			</div>
-			<WDGButton 
-				color="document-upload"
-				icon="address-card"
-				iconStyle="r"
-				:clickEvent="onChooseTypeEvent"
-				id="id"
-				>
+
+			<WDGButton
+			  v-if="step === 'choose-first-file-type' || firstDocumentType !== 'id'"
+			  color="document-upload"
+			  icon="address-card"
+			  iconStyle="r"
+			  :clickEvent="onChooseTypeEvent"
+			  id="id"
+			  >
 				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_ID') }}</slot>
 				<slot slot="description">{{ $t('account-authentication.user-documents.DESCRIPTOR_ID') }}</slot>
 			</WDGButton>
 
-			<WDGButton 
-				color="document-upload"
-				icon="passport"
-				:clickEvent="onChooseTypeEvent"
-				id="passport"
-				>
+			<WDGButton
+			  v-if="step === 'choose-first-file-type' || firstDocumentType !== 'passport'"
+			  color="document-upload"
+			  icon="passport"
+			  :clickEvent="onChooseTypeEvent"
+			  id="passport"
+			  >
 				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_PASSPORT') }}</slot>
 				<slot slot="description">{{ $t('account-authentication.user-documents.DESCRIPTOR_PASSPORT') }}</slot>
 			</WDGButton>
+
+			<WDGButton
+			  v-if="step === 'choose-second-file-type'"
+			  color="document-upload"
+			  icon="tax"
+			  :clickEvent="onChooseTypeEvent"
+			  id="tax"
+			  >
+				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_TAX') }}</slot>
+				<slot slot="description">{{ $t('account-authentication.user-documents.DESCRIPTOR_TAX') }}</slot>
+			</WDGButton>
+
+			<WDGButton
+			  v-if="step === 'choose-second-file-type'"
+			  color="document-upload"
+			  icon="welfare"
+			  :clickEvent="onChooseTypeEvent"
+			  id="welfare"
+			  >
+				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_WELFARE') }}</slot>
+			</WDGButton>
+
+			<WDGButton
+			  v-if="step === 'choose-second-file-type'"
+			  color="document-upload"
+			  icon="family"
+			  :clickEvent="onChooseTypeEvent"
+			  id="family"
+			  >
+				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_FAMILY') }}</slot>
+			</WDGButton>
+
+			<WDGButton
+			  v-if="step === 'choose-second-file-type'"
+			  color="document-upload"
+			  icon="birth"
+			  :clickEvent="onChooseTypeEvent"
+			  id="birth"
+			  >
+				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_BIRTH') }}</slot>
+			</WDGButton>
+
+			<WDGButton
+			  v-if="step === 'choose-second-file-type'"
+			  color="document-upload"
+			  icon="driving"
+			  :clickEvent="onChooseTypeEvent"
+			  id="driving"
+			  >
+				<slot slot="label">{{ $t('account-authentication.user-documents.TITLE_DRIVING') }}</slot>
+			</WDGButton>
 		</div>
 
-		<div class="upload-container" 
-		 	v-if="isDocumentChosen"
-			 >
+		<div
+		  v-else
+		  class="upload-container"
+		  >
 			<div class="title-container">
-				{{ $t('account-authentication.user-documents.SEND_ID') }} <br>
+				{{ $t('account-authentication.user-documents.SEND_YOUR') }}
+				<span v-if="lastDocumentTypeSelected === 'id'">{{ $t('account-authentication.user-documents.SEND_YOUR_ID') }}</span>
+				<span v-if="lastDocumentTypeSelected === 'passport'">{{ $t('account-authentication.user-documents.SEND_YOUR_PASSPORT') }}</span>
+				<span v-if="lastDocumentTypeSelected === 'tax'">{{ $t('account-authentication.user-documents.SEND_YOUR_TAX') }}</span>
+				<span v-if="lastDocumentTypeSelected === 'welfare'">{{ $t('account-authentication.user-documents.SEND_YOUR_WELFARE') }}</span>
+				<span v-if="lastDocumentTypeSelected === 'family'">{{ $t('account-authentication.user-documents.SEND_YOUR_FAMILY') }}</span>
+				<span v-if="lastDocumentTypeSelected === 'birth'">{{ $t('account-authentication.user-documents.SEND_YOUR_BIRTH') }}</span>
+				<span v-if="lastDocumentTypeSelected === 'driving'">{{ $t('account-authentication.user-documents.SEND_YOUR_DRIVING') }}</span>
+				(<a href="#userdocuments" @click="onChangeDocumentEvent">{{ $t('account-authentication.user-documents.SEND_ANOTHER') }}</a>)
+				<br>
 				{{ $t('account-authentication.user-documents.CHOOSE_DOCUMENT_TYPE') }}
 			</div>
 			<WDGRadioButton
-				name="nb-file"
-				value="1"
-				:groupValue="nbFile"
-				:onChange="onChooseNbFileEvent"
-				>
+			  name="nb-file"
+			  value="1"
+			  :onChange="onChooseNbFileEvent"
+			  >
 				<slot slot="label-after">{{ $t('account-authentication.user-documents.ONE_FILE') }}</slot>
 				<slot slot="description">{{ $t('account-authentication.user-documents.ONE_FILE_DESCRIPTOR') }}</slot>
 			</WDGRadioButton>
 			<WDGRadioButton
-				name="nb-file"
-				value="2"
-				:groupValue="nbFile"
-				:onChange="onChooseNbFileEvent"
-				>
-				<slot slot="label-after">{{ $t('account-authentication.user-documents.TWO_FILE') }}</slot>
-				<slot slot="description">{{ $t('account-authentication.user-documents.TWO_FILE_DESCRIPTOR') }}</slot>
+			  name="nb-file"
+			  value="2"
+			  :onChange="onChooseNbFileEvent"
+			  >
+				<slot slot="label-after">{{ $t('account-authentication.user-documents.TWO_FILES') }}</slot>
+				<slot slot="description">{{ $t('account-authentication.user-documents.TWO_FILES_DESCRIPTOR') }}</slot>
 			</WDGRadioButton>
-			<WDGUpload 
-		 	v-if="nbFiles>=1">
+			<div style="clear: both;"></div>
+
+			<WDGUpload
+			  id="file1"
+		 	  v-if="nbFileToDisplay >= 1"
+			  :onFileChange="onFileUploadChangeEvent"
+			  >
 			</WDGUpload>
-			<WDGUpload 
-		 	v-if="nbFiles>1">
+			<WDGUpload
+			  id="file2"
+		 	  v-if="nbFileToDisplay > 1"
+			  :onFileChange="onFileUploadChangeEvent"
+			  >
 			</WDGUpload>
+
+			<WDGButton
+			  v-if="isContinueButtonDisplayed"
+			  :clickEvent="onContinueButtonClickEvent"
+			  >
+				<slot slot="label">{{ $t('common.CONTINUE') }}</slot>
+			</WDGButton>
 		</div>
 
 		<WDGMascot type="side-1">
@@ -96,20 +174,100 @@ export default {
 	},
 	data () {
 		return {
+			step: 'choose-first-file-type',
+			nbFileToDisplay: 0,
 			firstDocumentType: '',
-			isDocumentChosen: false,
-			// isNbChosen: false,
-			nbFiles: 0
+			firstDocumentNbFiles: 0,
+			firstDocumentList: [],
+			secondDocumentType: '',
+			secondDocumentNbFiles: 0,
+			secondDocumentList: []
 		}
 	},
 	methods: {
 		onChooseTypeEvent: async function (...args) {
-			console.log(args[0].id)
-			this.isDocumentChosen = true
-			this.firstDocumentType = args[0].id
+			if ( this.step === 'choose-first-file-type' ) {
+				this.firstDocumentType = args[0].id
+				this.step = 'choose-first-file-count'
+			} else if ( this.step === 'choose-second-file-type' ) {
+				this.secondDocumentType = args[0].id
+				this.step = 'choose-second-file-count'
+			}
+		},
+		onChangeDocumentEvent () {
+			if ( this.step === 'choose-first-file-count' || this.step === 'choose-first-file-upload' ) {
+				this.firstDocumentType = ''
+				this.step = 'choose-first-file-type'
+			} else if ( this.step === 'choose-second-file-count' || this.step === 'choose-second-file-upload' ) {
+				this.secondDocumentType = ''
+				this.step = 'choose-second-file-type'
+			}
 		},
 		onChooseNbFileEvent (newValue) {
-			this.nbFiles = newValue
+			if ( this.step === 'choose-first-file-count' ) {
+				this.firstDocumentNbFiles = newValue
+				this.step = 'choose-first-file-upload'
+			} else if ( this.step === 'choose-second-file-count' ) {
+				this.secondDocumentNbFiles = newValue
+				this.step = 'choose-second-file-upload'
+			}
+			this.nbFileToDisplay = newValue
+		},
+		onFileUploadChangeEvent (idUploadDocument, files) {
+			let index = (idUploadDocument == 'file1') ? 0 : 1
+			if ( this.step === 'choose-first-file-upload' ) {
+				this.firstDocumentList.splice(index, 1, files)
+			} else if ( this.step === 'choose-second-file-upload' ) {
+				this.secondDocumentList.splice(index, 1, files)
+			}
+		},
+		onContinueButtonClickEvent () {
+			if ( this.step === 'choose-first-file-upload' ) {
+				this.step = 'choose-second-file-type'
+			} else if ( this.step === 'choose-second-file-upload' ) {
+				console.log('next')
+			}
+		}
+	},
+	computed: {
+		isFileTypeDisplayed () {
+			return (this.step === 'choose-first-file-type' || this.step === 'choose-second-file-type' )
+		},
+		isFileCountDisplayed () {
+			return (this.step === 'choose-first-file-count' || this.step === 'choose-second-file-count' )
+		},
+		isFileUploadDisplayed () {
+			return (this.step === 'choose-first-file-upload' || this.step === 'choose-second-file-upload' )
+		},
+		isContinueButtonDisplayed () {
+			console.log(this.step)
+			if ( this.step === 'choose-first-file-upload' ) {
+				for ( let i = 0; i < this.firstDocumentNbFiles; i++ ) {
+					if (this.firstDocumentList[ i ] === undefined) {
+						return false
+					}
+				}
+				return true
+			}
+			if ( this.step === 'choose-second-file-upload' ) {
+				console.log(this.secondDocumentNbFiles)
+				console.log(this.secondDocumentList)
+				for ( let i = 0; i < this.secondDocumentNbFiles; i++ ) {
+					if (this.secondDocumentList[ i ] === undefined) {
+						return false
+					}
+				}
+				return true
+			}
+			return false;
+		},
+		// retourne le type du document 1 ou 2 selon selui en cours de sélection
+		lastDocumentTypeSelected () {
+			if ( this.secondDocumentType !== '' ) {
+				return this.secondDocumentType
+			} else {
+				return this.firstDocumentType
+			}
 		}
 	}
 }
--- common/src/components/WDGUpload.vue
+++ common/src/components/WDGUpload.vue
@@ -1,49 +1,74 @@
 <template>
 	<div class="wdg-upload">
-			<WDGButton
-				color="document-upload"
-				icon="file-upload"
-				:clickEvent="onClickLocalEvent"
-				>
-				<slot slot="label">{{ $t('common.SEND_FILE') }}</slot>
-			</WDGButton>
-			{{ $t('common.SEND_FILE_DESCRIPTOR') }}
+		<label>
+			<input
+			  id="upload"
+			  name="attachment"
+			  type="file"
+			  @change="onFileChangeEvent"
+			  >
+			<span v-if="fileName !== ''">{{ fileName }}</span>
+			<span v-else>{{ $t('common.SEND_FILE') }}</span>
+		</label>
+		{{ $t('common.SEND_FILE_DESCRIPTOR') }}
 	</div>
 </template>
 
 <script>
-import WDGButton from '@/../../common/src/components/WDGButton'
 export default {
 	name: 'WDGUpload',
 	components: {
-		WDGButton
 	},
 	props: {
 		id: { type: String, default: null },
 		color: { type: String, default: 'red' },
 		name: { type: String, default: null },
-		clickEvent: Function
+		onFileChange: Function
+	},
+	data () {
+		return {
+			files: []
+		}
 	},
 	computed: {
+		fileName () {
+			if ((this.files.length > 0) && 'name' in this.files[0]) {
+				return this.files[0].name
+			}
+			return ''
+		}
 	},
 	methods: {
-		onClickLocalEvent () {
-			if (this.clickEvent !== undefined) {
-				this.clickEvent(this._props)
-			}
+		onFileChangeEvent (e) {
+			this.files = Array.from(e.target.files)
+			this.onFileChange(this.id, this.files)
 		}
 	}
 }
 </script>
 
 <style>
-button {
+.wdg-upload {
+	margin-top: 10px;
+}
+.wdg-upload label {
+	display: block;
 	width: 100%;
 	height: 48px;
 	margin-bottom: 16px;
 	padding: 0px;
+	text-align: center;
+	line-height: 48px;
 	text-transform: uppercase !important; /* ajout de important sinon c'est écrasé par normalize.less */
+	border: 1px dotted black;
 	cursor: pointer;
 }
-
+.wdg-upload label input[type=file] {
+	width: 0.1px;
+	height: 0.1px;
+	opacity: 0;
+	overflow: hidden;
+	position: absolute;
+	z-index: -1;
+}
 </style>
