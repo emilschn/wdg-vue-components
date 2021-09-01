1f26688b0575999bfc811a88ca1c8323b5cbabe1
Heloise <heloise.perat@etudiant.univ-lr.fr>
2021-09-01 12:14:17 +0200

    Mise en forme de la page de confirmation des documents

--- account-authentication/src/components/screen-checking/TheScreenInvestorChecking.vue
+++ account-authentication/src/components/screen-checking/TheScreenInvestorChecking.vue
@@ -1,18 +1,20 @@
 <template>
 	<div class="the-screen-investor-type">
-		<div>
+		<div class="title">
 			{{ $t('account-authentication.checking.DOCUMENTS_IMPORTED') }}
 		</div>
 
 		<WDGMascot
-		  type="face-1"
+		  type="face-4"
 		  alignMascot="left"
 		  :displayText="false"
 		  />
 		
+		<div class="documents-sent-description">
 		{{ $t('account-authentication.checking.YOUR_DOCUMENTS_MUST_BE_VALIDATED_BY_LEMONWAY') }}<br><br>
 
-		{{ $t('account-authentication.checking.YOUR_DOCUMENTS_CAN_BE_FOUND_IN_YOUR_ACCOUNT') }}<br><br>
+		{{ $t('account-authentication.checking.YOUR_DOCUMENTS_CAN_BE_FOUND_IN_YOUR_ACCOUNT') }}
+		</div>
 
 		<!-- Maintenant, nous allons compléter votre profil investisseur. Pour investir sur WE DO GOOD, il faut nous assurer que vous compreniez le risque de perte de votre investissement. -->
 	</div>
@@ -38,4 +40,24 @@ export default {
 </script>
 
 <style>
+div.the-screen-investor-type {
+	display: flex;
+	flex-flow: row wrap;
+	align-items: center;
+}
+div.the-screen-investor-type div.title {
+	font-family: 'Trebuchet MS', sans-serif;
+	font-size: 22px;
+	text-transform: uppercase;
+	text-align: center;
+	font-weight: bold;
+	margin-bottom: 40px;
+	width: 100%;
+}
+div.documents-sent-description {
+	background-color: #ebebeb;
+	padding: 40px;
+	width: 55%;
+	height: max-content;
+}
 </style>
